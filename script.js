b85ea1619144364c79e37bad49bd5f96b178a79d
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-01-20 14:53:32 +0300

    Finnish the postAdd function

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const loadedPosts =
-  localStorage.getItem("posts") === null
-    ? []
-    : JSON.parse(localStorage.getItem("posts"));
+let loadedPosts;
+
+const postForm = document.querySelector("#postForm");
 
 const load = async (postsNum) => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const posts = await response.json();
-    posts.length = postsNum;
+    // posts.length = postsNum;
 
-    localStorage.setItem("posts", JSON.stringify(posts));
-    console.log(response);
-
-    if (!response.ok) {
+    if (response.ok) {
+      loadedPosts =
+        localStorage.getItem("addedPosts") === null
+          ? posts
+          : JSON.parse(localStorage.getItem("addedPosts"));
+    } else {
       return new Error("Failed to fetch");
     }
   } catch (error) {
@@ -20,9 +21,12 @@ const load = async (postsNum) => {
   }
 };
 
-const showPosts = (Num) => {
-  load(Num);
+const showPosts = async (Num) => {
+  await load(Num);
 
+  const postsSection = document.querySelector("section.posts");
+  postsSection.innerHTML = "";
+  
   for (const post of loadedPosts) {
     // Make the structure for every post
     const postStructure = document.createElement("div");
@@ -34,10 +38,41 @@ const showPosts = (Num) => {
     postStructure.appendChild(bodyText);
 
     // Add the post to the posts section
-    const postsSection = document.querySelector("section.posts");
     postsSection.appendChild(postStructure);
     postsSection.innerHTML += "<button>Edit</button><button>Delete</button>";
   }
 };
+
+const addPost = async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(postForm);
+  const newPost = {
+    title: formData.get("title"),
+    body: formData.get("body")
+  };
+
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    });
+
+    if (response.ok) {
+      const postObj = await response.json();
+      loadedPosts.push(postObj);
+      localStorage.setItem("addedPosts", JSON.stringify(loadedPosts));
+      showPosts();
+      postForm.reset();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+postForm.addEventListener("submit", addPost)
 
 showPosts(10);
