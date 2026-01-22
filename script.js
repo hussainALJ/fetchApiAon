14ea560f431f8bd0aa337c8d1ec135857dd40d84
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-01-22 15:21:16 +0300

    Change post add form style & add comment section

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const url = "https://jsonplaceholder.typicode.com/posts";
 let loadedPosts = [];
 
 const nodes = {
+  body: document.querySelector("body"),
   form: document.querySelector("#postForm"),
   section: document.querySelector("section.posts"),
   main: document.querySelector("main"),
@@ -46,31 +47,70 @@ const api = {
     }
     return response.ok;
   },
+
+  async fetchComments(id, limit = 5) {
+    const response = await fetch (`${url}/${id}/comments?_limit=${limit}`);
+    if (!response.ok) "Failed to load comments";
+    return response.json();
+  }
 };
 
 // UI rendering
 const render = {
   post(postInfo) {
     const assay = document.createElement("div");
+    assay.id = postInfo.id;
     assay.classList = "hover assayStyle";
+
     const header = document.createElement("h1");
     header.classList = "title";
+    header.innerText = postInfo.title;
     const text = document.createElement("p");
     text.classList = "text";
-
-    header.innerText = postInfo.title;
     text.innerText = postInfo.body;
 
-    assay.id = postInfo.id;
-    assay.appendChild(header);
-    assay.innerHTML += "<hr>";
-    assay.appendChild(text);
-    assay.innerHTML += `<hr><button class='delete'><svg class='delete' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    const commentsBtn = document.createElement("h3");
+    commentsBtn.classList = "view";
+    commentsBtn.innerText = "View comments";
+    const commentSection = document.createElement("section")
+    commentSection.classList = "comments"
+    commentSection.appendChild(commentsBtn)
+    const assayFooter = document.createElement("div");
+    assayFooter.classList = "assayFooter";
+    assayFooter.appendChild(commentSection);
+    assayFooter.innerHTML += `<button class='delete'><svg class='delete' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect class='delete' x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="none" stroke="#000000"/>
         <line class='delete' x1="9.70711" y1="9.00002" x2="23" y2="22.2929" stroke="#000000" stroke-linecap="round"/>
         <line class='delete' x1="9" y1="22.2929" x2="22.2929" y2="9" stroke="#000000" stroke-linecap="round"/>
         </svg></button>`;
+
+    assay.appendChild(header);
+    assay.innerHTML += "<hr>";
+    assay.appendChild(text);
+    assay.innerHTML += '<hr>';
+    assay.appendChild(assayFooter);
     return assay;
+  },
+
+  viewComments (postId, commentsArr) {
+    const post = document.getElementById(`${postId}`);
+    const comments = post.querySelector(".comments")
+    const fragment = document.createDocumentFragment();
+    commentsArr.forEach((commentObj) => {
+      const name = document.createElement("h3");
+      name.innerText = commentObj.name;
+
+      const text = document.createElement("p");
+      text.innerText = commentObj.body;
+
+      const comment = document.createElement("div")
+      comment.id = parseInt(commentObj.id);
+      comment.classList = "comment"
+      comment.append(name, text);
+
+      fragment.appendChild(comment);
+    })
+    comments.appendChild(fragment);
   },
 
   allPosts(posts) {
@@ -96,6 +136,7 @@ const render = {
     nodes.separator.appendChild(form);
     nodes.main.appendChild(nodes.separator);
     nodes.editForm = document.querySelector("#editForm");
+    nodes.body.classList = "noScroll";
   },
 };
 
@@ -154,6 +195,16 @@ const editPost = async (e, id) => {
   }
 };
 
+const loadComments = async (id) => {
+  try {
+    const postComments = await api.fetchComments(id);
+    render.viewComments(id, postComments);
+  } catch (error) {
+    const post = document.getElementById(`${id}`);
+    post.innerText = "Failed to loadComments"
+  }
+}
+
 // Initialization
 const Initialization = async (num) => {
   try {
@@ -168,13 +219,16 @@ const Initialization = async (num) => {
 nodes.form.addEventListener("submit", addPost);
 
 nodes.section.addEventListener("click", (e) => {
-  const post = e.target.closest("div");
+  const post = e.target.closest(".assayStyle");
   switch (e.target.classList.value) {
     case "delete":
       deletePost(post.id, post);
       break;
     case "save":
       addPost(e);
+      break;
+    case "view":
+      loadComments(post.id);
       break;
     default:
       render.editForm(post);
@@ -184,10 +238,11 @@ nodes.section.addEventListener("click", (e) => {
 
 nodes.main.addEventListener("submit", (e) => {
     if (e.target.id === "editForm") {
-    let post = e.target.closest("div");
-    editPost(e, post.id);
-    post = post.closest(".separator");
-    post.remove();
+    let editMenu = e.target.closest("div");
+    editPost(e, editMenu.id);
+    editMenu = editMenu.closest(".separator");
+    editMenu.remove();
+    nodes.body.classList = "";
     }
 });
 
